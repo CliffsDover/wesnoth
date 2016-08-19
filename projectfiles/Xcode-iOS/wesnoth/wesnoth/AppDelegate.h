cf98851480b0d20c8f235ff4b2740d8b41c8eb4f
Cliffs Dover <bottle@dancingbottle.com>
2016-08-19 21:15:40 +0800

    compilation ok

---
+++ projectfiles/Xcode-iOS/wesnoth/wesnoth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wesnoth
+//
+//  Created by MyStaub on 2016/8/19.
+//  Copyright © 2016年 Dancing Bottle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ projectfiles/Xcode-iOS/wesnoth/wesnoth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wesnoth
+//
+//  Created by MyStaub on 2016/8/19.
+//  Copyright © 2016年 Dancing Bottle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
